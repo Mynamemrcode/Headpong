7d2153a89a1528d9e01d3b37d334084776f7774b
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-11-19 17:05:24 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,17 @@ function preload() {
 }
 function setup() {
     canvas = createCanvas(700,500);
-    canvas.center()
+    canvas.center();
+    video = createCapture(VIDEO);
+	video.parent("vid");
+  video.size(800,400);
+  posenet = ml5.poseNet(video, modeload);
+  posenet.on('pose', gotPoses);
+}
+function modeload() {
+	console.log("A log: Loaded");
+	
+}
+function draw() {
+
 }
