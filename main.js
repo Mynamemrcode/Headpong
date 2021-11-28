86e533bc9544c7cd912d9bdf958968c0b27d11ad
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-11-28 12:48:51 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightWristx = "";
+rightWristy = "";
+rightWristscore= "";
+status = "";
 function preload() {
 
 }
@@ -5,15 +9,24 @@ function setup() {
     canvas = createCanvas(700,500);
     canvas.center();
     video = createCapture(VIDEO);
-	video.parent("vid");
-  video.size(800,400);
+    video.hide();
+  
   posenet = ml5.poseNet(video, modeload);
   posenet.on('pose', gotPoses);
+}
+function srat() {
+    status="start";
+    document.getElementById("status").innerHTML = "Game Is Loading..."
 }
 function modeload() {
 	console.log("A log: Loaded");
 	
 }
+function gotPoses(results){
+    if(results.length > 0) {
+        rightWristx = results[0].pose.rightWrist.x;
+    }
+}
 function draw() {
-
+image(video, 0,0,700,500);
 }
